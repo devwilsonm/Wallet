f403508e547248e23683d7ebc0fc26222a4fc188
WilsonM <devwilsonm@gmail.com>
2021-05-29 01:13:14 -0500

    Correciones finales

--- Site.js
+++ Site.js
@@ -1,4 +1,5 @@
 var btnSearch = document.getElementById("btnSearch");
+var card = document.getElementById("cardDetalle");
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
@@ -16,11 +17,17 @@ document.addEventListener("DOMContentLoaded", function (event) {
 });
 
 function LoadDatatable(data){
-    $('#tabla').DataTable({
+    var dt = $('#tabla').DataTable({
         "processing": true,
         destroy: true,
         "data": data.results,
         "columns": [
+            {
+                "class":          "details-control",
+                "orderable":      false,
+                "data":           null,
+                "defaultContent": '<button type="button" class="btn btn-outline-info"><i class="ri-file-info-fill"></i></button>'
+            },
             { "data": "miner" },
             { "data": "land_id" },
             { "data": "rand1" },
@@ -33,4 +40,64 @@ function LoadDatatable(data){
         ]
     });
 
+    // Array to track the ids of the details displayed rows
+    var detailRows = [];
+ 
+    $('#tabla tbody').on( 'click', 'tr td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var row = dt.row( tr );
+        var idx = $.inArray( tr.attr('id'), detailRows );
+ 
+        if ( row.child.isShown() ) {
+            tr.removeClass( 'details' );
+            row.child.hide();
+ 
+            // Remove from the 'open' array
+            detailRows.splice( idx, 1 );
+        }
+        else {
+            tr.addClass( 'details' );
+            row.child( format( row.data() ) ).show();
+ 
+            // Add to the 'open' array
+            if ( idx === -1 ) {
+                detailRows.push( tr.attr('id') );
+            }
+        }
+    } );
+ 
+    // On each draw, loop over the `detailRows` array and show any child rows
+    dt.on( 'draw', function () {
+        $.each(detailRows, function ( i, id ) {
+            $('#'+id+' td.details-control').trigger( 'click' );
+        } );
+    } );
+}
+
+function format ( d ) {
+    let result="";
+    let sparams="";
+
+    card.querySelector(".card .card-header").innerText="params:";
+
+    for (const property in d.params) {
+        sparams +=''+`${property}: ${d.params[property]}`+'<br>';
+      }
+
+      card.querySelector(".card .card-text").innerHTML=sparams;
+      
+      result+=card.innerHTML;
+
+      card.querySelector(".card .card-header").innerText="template_data:";
+   
+      sparams="";
+      for (const property in d.template_data) {
+        sparams +=''+`${property}: ${d.template_data[property]}`+'<br>';
+      }
+
+      card.querySelector(".card .card-text").innerHTML=sparams;
+      
+      result+=card.innerHTML;
+
+    return '<div class="row">'+result+'</div>';
 }
